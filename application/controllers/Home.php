eca0c3b184fe99abca3504be63e177c6d989e3c6
Arturo Sánchez Fonseca <fonck.five@gmail.com>
2017-10-14 21:18:00 -0500

    set default time zone

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,21 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller
 {
 
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     *        http://example.com/index.php/welcome
-     *    - or -
-     *        http://example.com/index.php/welcome/index
-     *    - or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/user_guide/general/urls.html
-     */
+
+    public function __construct()
+    {
+        parent::__construct();
+        if (!ini_get('date.timezone')) {
+            date_default_timezone_set('GMT');
+        }
+    }
+
     public function index()
     {
         $data['content'] = 'contents/Home';
